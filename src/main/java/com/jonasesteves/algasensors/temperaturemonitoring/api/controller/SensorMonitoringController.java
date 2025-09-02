17460bb7207cfad79dcc7206afd6e2af19236a8f
Jonas Esteves <jonasestevess@gmail.com>
2025-09-02 12:31:41 +0100

    novas implementações

--- src/main/java/com/jonasesteves/algasensors/temperaturemonitoring/api/controller/SensorMonitoringController.java
+++ src/main/java/com/jonasesteves/algasensors/temperaturemonitoring/api/controller/SensorMonitoringController.java
@@ -7,6 +7,7 @@ import com.jonasesteves.algasensors.temperaturemonitoring.domain.repository.Sens
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/monitoring")
@@ -33,7 +34,9 @@ public class SensorMonitoringController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void enable(@PathVariable TSID sensorId) {
         SensorMonitoring sensorMonitoring = findByIdOrDefault(sensorId);
-
+        if (sensorMonitoring.getEnabled()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
         sensorMonitoring.setEnabled(true);
         sensorMonitoringRepository.saveAndFlush(sensorMonitoring);
     }
